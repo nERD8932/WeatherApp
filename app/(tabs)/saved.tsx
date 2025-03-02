42e12b204fa2954088c51696e1fb81ed46685b11
nerd8932 <samir.amin8932@gmail.com>
2025-03-02 01:27:40 -0400

    added comments and updated readme

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -6,54 +6,59 @@ import { Image } from 'react-native';
 import { useState, useEffect } from 'react';
 import { useIsFocused } from "@react-navigation/native";
 
+
+// Open connection to SQLite DB
 const db = SQLite.openDatabaseSync('weather.db')
 
 export default function Tab() {
+  // Using use state to define variabls used in render calls
   const [loading, setLoading] = useState(true);
   const [savedCities, setSavedCities] = useState([]);
-  const isFocused = useIsFocused();
+  const isFocused = useIsFocused(); // Check if tab is in focus
 
+  // Delete button to remove saved cities
   const deleteLocation = (id) => {
     db.execSync(`DELETE FROM locations WHERE id = ${id};`);
     setSavedCities(savedCities.filter(city => city.id !== id));
   };
 
+  // Get all existing rows
   useEffect(() => {
       (async () => {
         if(db!==null)
         {
           setLoading(false);
-          // db.execSync('CREATE TABLE IF NOT EXISTS "locations" ([id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, [city] NVARCHAR(256) NOT NULL);')
-          const rows = db.getAllSync("SELECT * FROM locations;")
-          setSavedCities(rows);
+          
+          try {
+            const rows = db.getAllSync("SELECT * FROM locations;")
+            setSavedCities(rows);
+          } catch (error) {
+            // Recreate table on error
+            db.execSync('CREATE TABLE "locations" ([id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, [city] NVARCHAR(256) NOT NULL);')
+            setSavedCities([]);
+          }
         }
       })();
-    }, []);
-    
-  useEffect(() => {
-    (async () => {
-      if(db!==null)
-      {
-        setLoading(false);
-        // db.execSync('CREATE TABLE IF NOT EXISTS "locations" ([id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, [city] NVARCHAR(256) NOT NULL);')
-        const rows = db.getAllSync("SELECT * FROM locations;")
-        setSavedCities(rows);
-      }
-    })();
-  }, [isFocused]);
-  
-  return (
-    <View style={styles.container}>
-      <Text style={styles.textPrimary}>Saved Locations</Text>
-      {savedCities.length > 0 && <FlatList
-                                      data={savedCities}
-                                      keyExtractor={(item) => item.id.toString()}
-                                      renderItem={({ item }) => (
-                                        <View style={styles.list}>
-                                          <Text style={styles.textSecondary}>{item.city}</Text>
-                                          <Button style={styles.delete} onPress={() => {deleteLocation(item.id)}}>🗑️</Button>
-                                        </View>
-                                      )}/>}
-    </View>
-  );
+    }, [isFocused]); // Run if tab is in focus
+
+     // Show loading screen before informaiton has been loaded
+    if (loading) {
+      return <ActivityIndicator color='#99cccc' style={styles.container} size="large" />;
+    }
+    else {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.textPrimary}>Saved Locations</Text>
+          {/* Create a Flat list of saved cities with delete buttons if the length of the list is not 0*/}
+          {savedCities.length > 0 && <FlatList
+                                          data={savedCities}
+                                          keyExtractor={(item) => item.id.toString()}
+                                          renderItem={({ item }) => (
+                                            <View style={styles.list}>
+                                              <Text style={styles.textSecondary}>{item.city}</Text>
+                                              <Button style={styles.delete} onPress={() => {deleteLocation(item.id)}}>🗑️</Button>
+                                            </View>
+                                          )}/>}
+        </View>
+  );}
 }
--- app/(tabs)/searchloc.tsx
+++ app/(tabs)/searchloc.tsx
@@ -5,23 +5,28 @@ import { Text, ActivityIndicator, TextInput, Button} from "react-native-paper";
 import { Image } from 'react-native';
 import { useState, useEffect } from 'react';
 
+// Open connection to SQLite DB
 const db = SQLite.openDatabaseSync('weather.db')
-//db.execSync("DROP TABLE IF EXISTS locations;")
 
 export default function Tab() {
+
+  // Using use state to define variabls used in render calls
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
   const [savedCities, setSavedCities] = useState([]);
   const [loading, setLoading] = useState(true);
   const [bg, setBG] = useState(require('./../../assets/images/day.png'))
   
+  // Fetch entered locations weather asynchronously, and set loading to true for system transparancy 
   const fetchWeather = async () => {
     setLoading(true)
+    // Get city coordinates, if city exists, else return
     const geoRes = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`);
     const geoData = await geoRes.json();
     if (!geoData.results) return;
-    
     const { latitude, longitude } = geoData.results[0];
+
+    // get and fetch results and set loading to false
     const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
     const weatherData = await weatherRes.json();
     setLoading(false)
@@ -29,6 +34,7 @@ export default function Tab() {
   };
 
   const saveLocation = () => {
+    // If input field is not empty
     if(city.length > 0)
     {
       try {
@@ -40,6 +46,7 @@ export default function Tab() {
     }
   };
 
+  // Set weather object to null if user changes the input field
   useEffect(() => {
     (async () => {
       if(weather!==null)
@@ -49,6 +56,7 @@ export default function Tab() {
     })();
   }, [city]);
 
+  // Once the weather object has been set, update the background sprite based on location's time of day
   useEffect(() => {
     (async () => {
       if(weather!==null)
@@ -60,8 +68,9 @@ export default function Tab() {
       }
       
     })();
-  }, [weather]);
+  }, [weather]); // Call only when weather is updated.
 
+  // Create table if not exists
   useEffect(() => {
       (async () => {
         if(db!==null)
@@ -72,32 +81,34 @@ export default function Tab() {
       })();
     }, []);
 
-    if (loading) {
-      return <ActivityIndicator color='#99cccc' style={styles.container} size="large" />;
-    }
-    else {
-      return (
-        <View style={styles.bg}>
-          <View style={styles.txtcontainersmall}>
-            <Text style={styles.textPrimary}>Location Weather</Text>
-          </View>
-          
-          <TextInput style={styles.textInput} placeholder="Enter city" onChangeText={setCity} />
-          <View style={styles.txtcontainer}>
-            
-            <Button style={styles.button} onPress={fetchWeather}> Search </Button>
-              {weather && <View style={styles.container}>
-                            <Image source={bg} style={styles.img}/>
-                            <View style={styles.txtcontainer}>
-                              <Text style={styles.textPrimary}>Current Weather in {city}</Text>
-                              <Text style={styles.textSecondary}>❄️ Temperature: {weather.temperature}°C</Text>
-                              <Text style={styles.textSecondary}>☴ Wind Speed: {weather.windspeed} km/h</Text>
-                            </View>
-                        </View>
-              }
-            <Button style={styles.button} onPress={saveLocation} disabled={savedCities.length >= 4}>Save Location</Button>
-          </View>
+
+  // Show loading screen before informaiton has been loaded
+  if (loading) {
+    return <ActivityIndicator color='#99cccc' style={styles.container} size="large" />;
+  }
+  else {
+    return (
+      <View style={styles.bg}>
+        <View style={styles.txtcontainersmall}>
+          <Text style={styles.textPrimary}>Location Weather</Text>
         </View>
-      );
-    }
+        
+        <TextInput style={styles.textInput} placeholder="Enter city" onChangeText={setCity} />
+        <View style={styles.txtcontainer}>
+          
+          <Button style={styles.button} onPress={fetchWeather}> Search </Button>
+            {weather && <View style={styles.container}>
+                          <Image source={bg} style={styles.img}/>
+                          <View style={styles.txtcontainer}>
+                            <Text style={styles.textPrimary}>Current Weather in {city}</Text>
+                            <Text style={styles.textSecondary}>❄️ Temperature: {weather.temperature}°C</Text>
+                            <Text style={styles.textSecondary}>☴ Wind Speed: {weather.windspeed} km/h</Text>
+                          </View>
+                      </View>
+            }
+          <Button style={styles.button} onPress={saveLocation} disabled={savedCities.length >= 4}>Save Location</Button>
+        </View>
+      </View>
+    );
+  }
 }
